42b191111d958eaef297f103ade8e08067845528
Aaron Janssen <aaron.m.janssen@ndsu.edu>
2023-06-13 10:25:21 -0500

    Deleted Plugins

--- coreplugins/editshortlinks/public/main.js
+++
@@ -1,7 +0,0 @@
-PluginsAPI.SharePopup.addLinkControl([
-        'editshortlinks/build/SLControls.js',
-        'editshortlinks/build/SLControls.css'
-    ],function(args, SLControls){
-        return SLControls;
-    }
-);
--- coreplugins/shortlinks/public/main.js
+++
@@ -1,7 +0,0 @@
-PluginsAPI.SharePopup.addLinkControl([
-        'shortlinks/build/SLCheckbox.js',
-        'shortlinks/build/SLCheckbox.css'
-    ],function(args, SLCheckbox){
-        return SLCheckbox;
-    }
-);
